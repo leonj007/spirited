11739bc9ce6de36f3cb689833703fa596cbb4ef7
leonj007 <ljswift@me.com>
2014-08-19 22:31:05 -0400

    book details

--- app/models/BookDetails.scala
+++ app/models/BookDetails.scala
@@ -1,9 +1,15 @@
-sealed abstract class Constraint(val name: String, val verifier: Int => Boolean)
+sealed abstract class Testament(val name: String)
 
-case object NotTooBig extends Constraint("NotTooBig", (_ < 1000))
-case object NonZero extends Constraint("NonZero", (_ != 0))
-case class NotEquals(x: Int) extends Constraint("NotEquals " + x, (_ != x))case class Book(name: String, synopsis: String, testament: Testament)
+case object OldTestament extends Testament("Old Testament")
+case object NewTestamnet extends Testament("New Testament")
+case class Book(name: String, synopsis: String, testament: Testament)
 
-class BookDetails(bookmeta: Book){
+class BookDetails(bookmeta: Book) {
+
+  val name: String = bookmeta.name
+
+
+
+
 
 }
